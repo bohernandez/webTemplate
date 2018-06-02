1c094762d22b31bf98939cadd04cb09ad23918fc
bohernandez <boris.hceron@gmail.com>
2018-06-02 06:44:29 -0600

    Adding hero to HomePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,30 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import SignUp from '../components/signUp';
+import SignUp from '../components/signUp'
+import './index.scss'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <Link to="/about-me/">About</Link>
-    <p>.</p>
-    <Link to="/contact/">Contact</Link>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <SignUp />
+    <header className="hero-container">
+      <div className='container'>
+        <h6 className='hero-container__intro'>Introduccion</h6>
+        <h2 className='hero-container__title'>Photography is 
+          a journey</h2>
+        <p className="hero-container__description">
+          My advice to everyone is to create a life, not to consume it. I'm curious to
+          see how many emotions now appear in things created with the help of 
+          complex technologies. They are so loaded with different feelings that it
+          is no longer possible to separate emotions and work from the
+          instrument on which it is done.
+        </p>
+        <a href='#' className='hero-container__link'>Discover</a>
+      </div>
+    </header>
+
+    <section>
+      <div>About Us</div>
+    </section>
+    
   </div>
 )
 
