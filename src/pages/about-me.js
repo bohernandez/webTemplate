abbb09430f7954907fedca4e776cb2156628a5af
bohernandez <boris.hceron@gmail.com>
2018-06-08 22:29:47 -0600

    Adding summary to about us page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -19,9 +19,50 @@ const AboutMe = () => (
       </div>
     </section>
 
+    <section className="info-container bg-dark-custom">
+      <div className="container">
+        <div className="row">
+        <div className="col-sm-6 col-md-4">
+					<h3 className="info-container__title">Experience.</h3>
+				    <ul className="info-container__list list-info list-unstyled">
+					    <li><strong>Photo Agency Shutter</strong></li>
+						<li>Photographer-intern <br /> 2016–2017</li>
+				    </ul>
+				    <ul className="info-container__list list-unstyled">
+					    <li><strong>Studio e.Oliver</strong></li>
+						<li>Photographer-retoucher <br />2017–Present</li>
+				    </ul>
+				</div>
+        <div className="col-sm-6 col-md-4">
+					<h3 className="info-container__title">Education.</h3>
+				    <ul className="info-container__list list-info list-unstyled">
+					    <li><strong>Photography School</strong></li>
+						<li>New York Film Academy <br /> 2010–2012</li>
+				    </ul>
+				    <ul className="info-container__list list-unstyled">
+					    <li><strong>Photography Universities</strong></li>
+						<li> Bloomfield Hills, <br />MI 2013–2015</li>
+				    </ul>
+				</div>
+        <div className="col-sm-6 col-md-4">
+					<h3 className="info-container__title">Equipment.</h3>
+				    <ul className="info-container__list list-info list-unstyled">
+					    <li><strong>Digital cameras</strong></li>
+						<li>Canon EOS 5D Mark IV <br />Sony Alpha A7R II</li>
+				    </ul>
+				    <ul className="info-container__list list-unstyled">
+					    <li><strong>Video camera</strong></li>
+						<li>Sony AX53 4K Handycam <br />4K 24p Cinema/60p </li>
+				    </ul>
+				</div>
+        </div>
+      </div>
+    </section>
+
+    
 
     <section className="video-container bg-dark-custom">
-      <iframe className="video-container__iframe" width="560" height="625" src="https://www.youtube.com/embed/hW9PUzl7j9w?rel=0&amp;controls=0&amp;showinfo=0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <iframe className="video-container__iframe" width="560" height="625" src="https://www.youtube.com/embed/hW9PUzl7j9w?rel=0&amp;controls=0&amp;showinfo=0" frameBorder="0" allowFullScreen></iframe>
     </section>
 
     <SignUp />
